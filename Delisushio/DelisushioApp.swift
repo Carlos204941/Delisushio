28ed31113abdad863a4d21fed7aebf4b0b90e941
Carlos Martinez <calinmtzhdz3@icloud.com>
2022-02-25 08:03:25 -0500

    Initial Commit

---
+++ Delisushio/DelisushioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelisushioApp.swift
+//  Delisushio
+//
+//  Created by Carlos Martinez on 2022-02-25.
+//
+
+import SwiftUI
+
+@main
+struct DelisushioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
